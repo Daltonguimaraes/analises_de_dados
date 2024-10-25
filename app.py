98d3860dc9c73798cb5c341f33e7558af2b4b614
Dalton Guimaraes <daltonguim@gmail.com>
2024-10-24 20:58:25 -0400

    submit da lista de ações através do template

--- app.py
+++ app.py
@@ -1,27 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from models.shearch import Getdata
 from models.read_data import Lerdados
 
 app = Flask(__name__)
 
-# Cria a lista inicial de ações e obtém os dados
-inv = Getdata(list_actions=['^BVSP', 'CSMG3.SA', 'BBAS3.SA', 'CPFE3.SA', 'BRAP4.SA', 'BBSE3.SA'])
-inv.date()
-inv.tickets()
-inv.symbols()
-
-# Obter lista de ações através do Lerdados
-ler_dados = Lerdados()
-tickets, dados_acoes = ler_dados.listactions()
-df_transformado = ler_dados.transformacao(tickets, dados_acoes)
-
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    # Exibir a lista de ações disponíveis
-    return render_template('index.html', tickets=tickets)
+    if request.method == 'POST':
+        # Recebe a lista de ações do formulário
+        actions_list = request.form.get('actions_list')
+        # Divide a string em uma lista de ações, removendo espaços extras
+        actions = [action.strip() for action in actions_list.split(',')]
+        
+        # Cria o objeto Getdata com a lista de ações fornecida pelo usuário
+        inv = Getdata(list_actions=actions)
+        inv.date()
+        inv.tickets()
+        inv.symbols()
+
+        # Redireciona para a página de transformação após a consulta
+        return redirect(url_for('transform'))
+    
+    return render_template('index.html')
 
 @app.route('/transform')
 def transform():
+    # Obter lista de ações através do Lerdados
+    ler_dados = Lerdados()
+    tickets, dados_acoes = ler_dados.listactions()
+    df_transformado = ler_dados.transformacao(tickets, dados_acoes)
+
     # Exibir a transformação dos dados
     return render_template('transform.html', data=df_transformado.to_dict(orient='records'))
 
