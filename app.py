d2720fe27a2409224fa5ad005983b8523418b7cd
Dalton Guimaraes <daltonguim@gmail.com>
2024-11-03 20:12:21 -0400

    Ajustes no dashboard

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for
 from models.shearch import Getdata
 from models.read_data import Lerdados
+import plotly.express as px
+import pandas as pd
+
 
 app = Flask(__name__)
 
+# Index
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
@@ -19,10 +23,11 @@ def index():
         inv.symbols()
 
         # Redireciona para a página de transformação após a consulta
-        return redirect(url_for('transform'))
+        return redirect(url_for('dashboard'))
     
     return render_template('index.html')
 
+# Apresentação dos dados transformados 
 @app.route('/transform')
 def transform():
     # Obter lista de ações através do Lerdados
@@ -32,6 +37,26 @@ def transform():
 
     # Exibir a transformação dos dados
     return render_template('transform.html', data=df_transformado.to_dict(orient='records'))
+
+# Painel de gráficos com dados
+@app.route('/dashboard')
+def dashboard():
+    # Carrega o arquivo CSV
+    df = pd.read_csv('./data/dados_adj_close.csv', sep=';', encoding='utf-8')
+    
+    # Cria um gráfico de linhas usando Plotly
+    fig = px.area(
+        df,
+        x='Date',  # Coluna de datas
+        y=df.columns[1:],  # Todas as colunas de ações, excluindo 'Date'
+        title='Gráfico de Linhas das Ações'
+    )
+
+    # Converte o gráfico para HTML
+    graph_html = fig.to_html(full_html=False)
+
+    # Passa o gráfico para o template
+    return render_template('dashboard.html', graph_html=graph_html)
 
 if __name__ == '__main__':
     app.run(debug=True)
