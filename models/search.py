31e71d4376baef3c46eb1607105a69ab6ea68545
Dalton Guimaraes <daltonguim@gmail.com>
2025-04-04 18:57:47 -0400

    dados ipca

--- models/search.py
+++ models/search.py
@@ -2,6 +2,7 @@ import datetime
 import pandas as pd
 import yfinance as yf
 import os
+import requests
 
 class Getdata:  
     def __init__(self, period="1y", interval="1d", list_actions=None):
@@ -38,3 +39,36 @@ class Getdata:
         print(f'Dados salvos em: data/symbols/{dir}, com o nome {file}.csv')
         print("----------------------------------------------------------------------------------")
         return dados_completos
+
+    def getipca(self):
+        # Código da série do IPCA
+        codigo_serie = 16121
+
+        # Datas: dataInicial deve ser MENOR que dataFinal
+        dataInicial = datetime.date(2024, 4, 2)  # Exemplo fixo, igual ao da sua URL
+        dataFinal = datetime.date(2025, 4, 2)    # Exemplo fixo
+
+        # Converte para formato dd/mm/yyyy exigido pela API
+        dataInicial_str = dataInicial.strftime('%d/%m/%Y')
+        dataFinal_str = dataFinal.strftime('%d/%m/%Y')
+
+        # Monta URL
+        url = (
+        f'https://api.bcb.gov.br/dados/serie/bcdata.sgs.{codigo_serie}/dados'
+        f'?formato=json&dataInicial={dataInicial_str}&dataFinal={dataFinal_str}')
+
+        # Requisição
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            dados = response.json()
+            if dados:
+                df_ipca = pd.DataFrame(dados)
+                df_ipca.to_csv('./data/dados_ipca.csv', sep=';', encoding='utf-8', index=False)
+                return df_ipca
+            else:
+                print("A API retornou uma lista vazia.")
+                return pd.DataFrame()
+        else:
+            print(f'Erro na requisição: {response.status_code}')
+            return pd.DataFrame()
